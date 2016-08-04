18344a834581e665c5a75debfdf668dc336ca2a1
Patrick Lipscomb and Sami Al-Jamal <sami@>
2016-08-04 11:39:32 -0700

    add the crime data linked via fusion tables.

--- js/gmaps-interface.js
+++ js/gmaps-interface.js
@@ -2,16 +2,39 @@ var pdxMap = require('./../js/gmaps.js').mapModule;
 var basemap;
 
 $(document).ready(function() {
+  $('#neighborhood-toggleFalse').hide();
+  $('#crimeMap-toggleFalse').hide();
   basemap = new pdxMap();
   var mapObject = basemap.generateMap();
-
   var infoWindow = basemap.generateInfoWindow();
   var neighborhood = basemap.generateNeighborhood(infoWindow, mapObject);
+  var crimePointMap;
 
   $('#neighborhood-toggleTrue').click(function() {
     basemap.showneighborhood(neighborhood, mapObject)
+    $('#neighborhood-toggleFalse').show();
+    $('#neighborhood-toggleTrue').hide();
   });
   $('#neighborhood-toggleFalse').click(function() {
-    basemap.hideneighborhood(neighborhood)
+    basemap.hideneighborhood(neighborhood);
+    $('#neighborhood-toggleTrue').show();
+    $('#neighborhood-toggleFalse').hide();
   });
+  $('#crimeMap-toggleTrue').click(function() {
+    var crime = "Prostitution"
+    basemap.generateHeatMap(mapObject, crime)
+    $('#crimeMap-toggleFalse').show();
+    $('#crimeMap-toggleTrue').hide();
+  });
+  $('#crime-form').submit(function(event) {
+    event.preventDefault();
+    var crime = $('#crime-selection').val()
+    crimePointMap = basemap.generateCrimePoints(crime)
+    crimePointMap.setMap(mapObject)
+  })
+  $('#crimePointMap-toggleFalse').click(function() {
+    crimePointMap.setMap(null)
+  });
+
+
 });
--- js/gmaps.js
+++ js/gmaps.js
@@ -21,12 +21,12 @@ pdxMap.prototype.generateInfoWindow = function(){
 pdxMap.prototype.generateNeighborhood = function(infoWindow, mapObject){
   var neighborhood = new google.maps.Data();
   neighborhood.loadGeoJson('portland.json');
-  neighborhood.addListener('click', function(e) {
-    neighborhood.overrideStyle(e.feature, {
-     strokeColor: '#2a2a2a',
-     strokeWeight: 2
-   });
-  });
+  // neighborhood.addListener('click', function(e) {
+  //   neighborhood.overrideStyle(e.feature, {
+  //    strokeColor: '#2a2a2a',
+  //    strokeWeight: 0
+  //  });
+  // });
   neighborhood.addListener('click', function(e) {
     console.log(e);
     infoWindow.setContent('<div style="line-height:1.00;overflow:hidden;white-space:nowrap;">' +
@@ -37,8 +37,10 @@ pdxMap.prototype.generateNeighborhood = function(infoWindow, mapObject){
     infoWindow.open(mapObject, anchor);
   });
   neighborhood.setStyle({
-    strokeColor: 'blue',
-    strokeWeight: 2
+    strokeColor: '#2a2a2a',
+    strokeOpacity: '0.7',
+    fillOpacity: '0.2',
+    strokeWeight: 0.6
   });
   return neighborhood
 };
@@ -49,6 +51,18 @@ pdxMap.prototype.showneighborhood = function(layer, map){
 
 pdxMap.prototype.hideneighborhood = function(layer){
   layer.setMap(null)
+};
+
+pdxMap.prototype.generateCrimePoints = function(crime){
+  var crimeMap = new google.maps.FusionTablesLayer({
+    query: {
+      select: 'Latitude',
+      from: '1cw-Qrd1pGwELM4vkQM_94C6ajrAsJftCf5kvnWrz',
+      where: "'Major Offense Type' = '" + crime + "'",
+    },
+    styleID: 2
+  });
+  return crimeMap
 };
 
 
